f5d3a577a43228e25952193fd98b90cd42b66044
VenkatUnnam <109094839+VenkatUnnam@users.noreply.github.com>
2025-01-04 18:17:01 +0530

    deploy all changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import EncoderDecoder from './EncoderDecoder';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <EncoderDecoder />
+        </header>
+      </div>
+  );
+}
+
+export default App
